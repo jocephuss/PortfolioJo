5e48bf46b71c5dce6b7096da677920840f16e3a4
Jocephuss <rowlandjosiah14@gmail.com>
2024-09-21 11:08:28 -0600

    "About me Header and footer code added"

---
+++ Portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img />
+      <p>
+        Hello, my name is Josiah Rowland I am a full stack web developer student
+        at Denver University. My goal is to expand my knowledge through in field
+        experience with clients, live feedback and finished products. looking to
+        perfect my skills and growing vision in the world of development.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/jocephuss">GitHub</a>
+      <a href="https://www.linkedin.com/in/josiah-rowland-2118a931b/">
+        LinkedIn
+      </a>
+      <a href="mailto:rowlandjosiah14@gmail.com">Stack Overflow</a>
+    </footer>
+  );
+};
+
+export default Footer;
