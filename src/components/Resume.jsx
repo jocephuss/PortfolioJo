b013231a721325d1a41315963d8a8bde982f64b4
Jocephuss <rowlandjosiah14@gmail.com>
2024-09-23 18:45:10 -0600

    ""

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -117,7 +117,7 @@ const Resume = () => {
             Development
           </li>
           <li>Databases: PostgresSQL, MongoDB.</li>
-          <li>Tools & Platforms: Git, GitHub, VS Code, NPM, Jest</li>
+          <li>Tools & Platforms: Git, GitHub, VS Code, NPM, Jest, graphQL</li>
           <li>
             Other Skills: REGEX, Agile Methodologies, Version Control, MERN
             stack.
