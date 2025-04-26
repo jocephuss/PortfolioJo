e5da52b7359486ac2e293f0a1b76de1e2c5b97b1
Jocephuss <rowlandjosiah14@gmail.com>
2025-04-26 15:59:15 -0600

    "no need for index.css"

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Import the root App component
-import "./index.css"; // Global styles
+import App from "./App";
 
 // Render the App component inside the root div in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
